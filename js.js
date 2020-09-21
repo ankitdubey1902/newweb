7673b1ea02aac3c4e5f7f18d826a51d12db739ae
unknown <ankitdubey1902@gmail.com>
2020-09-21 20:56:33 +0530

    second commit

--- js.js
+++ js.js
@@ -5,14 +5,15 @@
 // document.getElementById("ip").innerHTML = "Your Ip :" + data.query;
 // document.getElementById("location").innerHTML = "Your Ip :" + data.city;
 
-// https://ipapi.co/json/y
-fetch("http://ip-api.com/json")
+// https://ipapi.co/json/
+// http://ip-api.com/json
+fetch("https://ipapi.co/json/")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    document.getElementById("ip").innerHTML = data.query;
+    document.getElementById("ip").innerHTML = data.ip;
     document.getElementById("location").innerHTML =
       data.country + " " + data.city;
   });
